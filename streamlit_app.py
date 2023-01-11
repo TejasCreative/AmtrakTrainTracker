55b08bb2c06fc5791a6a99c7dd52212f90fe575d
Tejas Bhartiya <32378536+TejasCreative@users.noreply.github.com>
2023-01-10 23:01:43 -0800

    Refactor

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,12 @@ import pytz
 from dateutil import tz
 import time
 
-
+def time_convert(time):
+    time = time[11:19]                
+    newtime = tz.tzlocal()               
+    newtime = datetime.strptime(time, "%H:%M:%S")
+    newtime = newtime.strftime("%I:%M:%S %p")  
+    return newtime
 
 timezone_storage = {
     -12: "DST(UTC-12)",
@@ -46,80 +51,44 @@ timezone_storage = {
     12: "FST(UTC+12)",
 }
 
-
 get5 = "https://api.amtraker.com/v1/trains/5"
 response5 = requests.get(get5)
-
 data5 = json.loads(response5.text)
-
 lastValTS = data5[0]['lastValTS']
 lastValTS = lastValTS[11:19]
 local_zone = tz.tzlocal()
 local_dt = datetime.strptime(lastValTS, "%H:%M:%S")
-
 from_zone = tz.tzutc()
 to_zone = tz.tzlocal()
-
-
 utc = local_dt.replace(tzinfo=from_zone)
 local_dt = utc.astimezone(to_zone)
-
 timezone_calc = (str(local_dt))
-
 timezone_calc = timezone_calc[19:22]
 timezone_calc_int = int(timezone_calc)
-
-
-
 timezone = timezone_storage[timezone_calc_int]
-
 
 st.title("Amtrak Train Tracker")
 number = st.number_input('Train Number:', value = 715, step = 1)
 now = datetime.now()
 current_time = now.strftime("%I:%M:%S %p")
 st.caption("Currently it is " + current_time + " " + timezone)
-
 url = "https://api.amtraker.com/v1/trains/" + str(number)
-
 response = requests.get(url)
-
-
 
 if(response.status_code == 500):
     st.write("There is no train with that number.")
     st.stop()
 
-
-
 data = json.loads(response.text)
 if(len(data) == 0 ):
     st.write("That train is not running currently")
     st.stop()
-latitude = data[0]['lat']
-
-
-
 trainCount = len(data)
-
-
 routeName = data[0]['routeName']
 if(trainCount == 1):
     st.write("There is 1 train on the " + routeName + " route.")
 else:
     st.write("There are " + str(trainCount) + " trains on the " + routeName + " route.")
-
-
-
-
-
-
-    
-
-
-
- 
-
 
 
 for i in range(trainCount):
@@ -137,27 +106,14 @@ for i in range(trainCount):
     longitudeRounded = round(longitude, 5)
     trainState = data[i]['trainState']
 
-    # lastValTS = lastValTS[11:23]
-
     lastValTS = lastValTS[11:19]
     local_zone = tz.tzlocal()
     local_dt = datetime.strptime(lastValTS, "%H:%M:%S")
-
     from_zone = tz.tzutc()
     to_zone = tz.tzlocal()
-
-
     utc = local_dt.replace(tzinfo=from_zone)
     local_dt = utc.astimezone(to_zone)
 
-    timezone_calc = (str(local_dt))
-
-    timezone_calc = timezone_calc[19:22]
-    timezone_calc_int = int(timezone_calc)
-
-
-
-    timezone = timezone_storage[timezone_calc_int]
 
     velocityrounded = round(velocity, 2)
     if(trainState != "Completed"):
@@ -197,51 +153,22 @@ for i in range(trainCount):
                 if('postDep' not in stationinfo[i]):
                     trainline = trainline + "Scheduled Departure is at " + stationinfo[i]['schDep']
                 else:
-                    deptiming = stationinfo[i]['postDep']
-                    deptiming = deptiming[11:19]
-                    deptiming_dt = tz.tzlocal()
-                    deptiming_dt = datetime.strptime(deptiming, "%H:%M:%S")
-                    deptiming_dt = deptiming_dt.strftime("%I:%M:%S %p")
-
+                    deptiming_dt =  time_convert(stationinfo[i]['postDep'])
                     trainline = trainline + "Departed at " + str(deptiming_dt) + ", " + stationinfo[i]['postCmnt'].lower()
 
             else:
                 if('estArr' in stationinfo[i]):
-                    deptiming = stationinfo[i]['estArr']
-                    deptiming = deptiming[11:19]
-                    deptiming_dt = tz.tzlocal()
-                    deptiming_dt = datetime.strptime(deptiming, "%H:%M:%S")
-                    deptiming_dt = deptiming_dt.strftime("%I:%M:%S %p")
+                    deptiming_dt =  time_convert(stationinfo[i]['estArr'])
                     trainline = trainline + "Estmiated Arrival is at " + str(deptiming_dt) + ", " + stationinfo[i]['estArrCmnt'].lower()
                 elif('estDep' in stationinfo[i]):
-                    deptiming = stationinfo[i]['postArr']
-                    deptiming = deptiming[11:19]
-                    deptiming_dt = tz.tzlocal()
-                    deptiming_dt = datetime.strptime(deptiming, "%H:%M:%S")
-                    deptiming_dt = deptiming_dt.strftime("%I:%M:%S %p")
-
-                    v_deptiming = stationinfo[i]['postArr']
-                    v_deptiming = deptiming[11:19]
-                    v_deptiming_dt = tz.tzlocal()
-                    v_deptiming_dt = datetime.strptime(v_deptiming, "%H:%M:%S")
-                    v_deptiming_dt = v_deptiming_dt.strftime("%I:%M:%S %p")
-
+                    deptiming_dt =  time_convert(stationinfo[i]['postArr'])
+                    v_deptiming_dt =  time_convert(stationinfo[i]['estDep'])
 
                     trainline = trainline + "Arrived at " + str(deptiming_dt) + " and estmiated departure is at " + str(v_deptiming_dt) + ', ' + stationinfo[i]['estDepCmnt'].lower()
                 elif('postDep' in stationinfo[i]):
-                    deptiming = stationinfo[i]['postDep']
-                    deptiming = deptiming[11:19]
-                    deptiming_dt = tz.tzlocal()
-                    deptiming_dt = datetime.strptime(deptiming, "%H:%M:%S")
-                    deptiming_dt = deptiming_dt.strftime("%I:%M:%S %p")
+                    deptiming_dt =  time_convert(stationinfo[i]['postDep'])
                     trainline = trainline + "Departed at " + str(deptiming_dt) + ", " + stationinfo[i]['postCmnt'].lower()
                 else:
                     trainline = trainline + "No Data"
-
-
-
-
-
-
 
             st.write(trainline)
